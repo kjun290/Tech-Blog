38b2b57b0fb2111aecab6ef568051351b8812fd2
Kevin Jun <kwjun90@gmail.com>
2021-06-08 16:43:14 -0500

    completed homeroute

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,4 +3,8 @@ const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes.js');
 const commentRoutes = require('./comment-routes.js');
 
+router.use.apply('/user', userRoutes);
+router.use.apply('/post', postRoutes);
+router.use.apply('/comment', commentRoutes);
 
+module.exports = router;
---
+++ controllers/home-routes.js
@@ -0,0 +1,60 @@
+const router = require("express").Router();
+const {post, Comment, User} = require('../models');
+
+
+// get all posts for the homepage
+router.get('/', (req, res) => {
+    post.findAll({
+        include: [User],
+    })
+    .then((dbPostData) => {
+        const posts = dbPostData.map((post) => post.get({ plain: true}));
+
+        res.render("all-posts", {posts});
+    })
+    .catch((err) => {
+        res.status(500).json(err);
+    });
+});
+
+// get a single post 
+router.get('/post/:id', (req,res) => {
+    post.findByPk(req.params.id, {
+        include: [
+            User,
+            {model: Comment, 
+            include: [User],},
+        ],
+    })
+    .then((dbPostData) => {
+        if(dbPostData) {
+            const post = dbPostData.get({plain:true});
+            res.render('single-post', {post});
+        }
+        else {
+            res.status(404).end();
+        };
+    })
+    .catch((err) => {
+        res.status(500).json(err);
+    });
+});
+
+
+router.get('/login', (req,res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+
+router.get('/signup', (req,res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('signup');
+});
+
+module.exports = router;
