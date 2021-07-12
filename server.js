5e5a4f7a78d9d829937ca3e951813f88a606909a
Kevin Jun <kwjun90@gmail.com>
2021-07-12 16:30:35 -0700

    deploying to heroku again

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({ helpers });
+
 
 const sess = {
   secret: 'Super secret secret',
@@ -25,9 +25,9 @@ const sess = {
 };
 
 app.use(session(sess));
-
+const hbs = exphbs.create({ helpers });
 // Inform Express.js on which template engine to use
-app.engine('handlebars', hbs.engine);
+app.engine("handlebars", hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
